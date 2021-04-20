b394b350754e7191ffb2863bfb65d4a088e38370
Jeffrey Tan s3851781 <s3851781@student.rmit.edu.au>
2021-04-21 00:18:59 +1000

    Qwirkle.cpp now uses "using" + welcome message added.

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -5,52 +5,67 @@
 #include <exception>
 #include <limits>
 
-#define EXIT_SUCCESS    0
+#define EXIT_SUCCESS 0
 
-void runMenu(int userChoice, bool* stop);
+using std::cin;
+using std::cout;
+using std::endl;
 
-int main(void) {
-   LinkedList* list = new LinkedList();
+void runMenu(int userChoice, bool *stop);
+
+int main(void)
+{
+   cout << "Welcome to Qwirkle!" << endl;
+   cout << "-------------------" << endl;
+
+   LinkedList *list = new LinkedList();
    delete list;
    bool stop = false;
-   bool* stopPtr = &stop;
+   bool *stopPtr = &stop;
    int userChoice;
-   while(!stop) {
-      std::cout << "Menu" << std::endl;
-      std::cout << "---" << std::endl;
-      std::cout << "1. New Game" << std::endl;
-      std::cout << "2. Load game" << std::endl;
-      std::cout << "3. Credits (Show student information)" << std::endl;
-      std::cout << "4. Quit" << std::endl;
-      std::cout << "> ";
-      std::cin >> userChoice;
-      while(std::cin.fail()) {
-         std::cout <<"Invalid input, please try again \n> ";
-         std::cin.clear();
-         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-         std::cin >> userChoice;
+   while (!stop)
+   {
+      cout << "Menu" << endl;
+      cout << "---" << endl;
+      cout << "1. New Game" << endl;
+      cout << "2. Load game" << endl;
+      cout << "3. Credits (Show student information)" << endl;
+      cout << "4. Quit" << endl;
+      cout << "> ";
+      cin >> userChoice;
+      while (cin.fail())
+      {
+         cout << "Invalid input, please try again \n> ";
+         cin.clear();
+         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+         cin >> userChoice;
       }
       runMenu(userChoice, stopPtr);
    }
    return EXIT_SUCCESS;
 }
 
-void runMenu(int userChoice, bool* stop) {
-   if(userChoice==1) {
-     //New game
-     std::cout << "Choice 1 selected \n \n";
+void runMenu(int userChoice, bool *stop)
+{
+   if (userChoice == 1)
+   {
+      //New game
+      cout << "Choice 1 selected \n \n";
    }
-   else if(userChoice==2) {
+   else if (userChoice == 2)
+   {
       //Load game
-      std::cout << "Choice 2 selected \n \n";
+      cout << "Choice 2 selected \n \n";
    }
-   else if(userChoice==3) {
+   else if (userChoice == 3)
+   {
       //Credits
-      std::cout << "Choice 3 selected \n \n";
+      cout << "Choice 3 selected \n \n";
    }
-   else if(userChoice==4) {
+   else if (userChoice == 4)
+   {
       //Quit
-      std::cout << "Goodbye" << std::endl;
+      cout << "Goodbye" << endl;
       *stop = true;
    }
 }
