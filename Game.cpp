369f428130613289c6d763f1b7e8bf953a311c73
[Sidhra Fernando-Plant s3873756] <s3873756@student.rmit.edu.au>
2021-04-26 23:36:06 +1000

    Start while loop for gameplay

--- Game.cpp
+++ Game.cpp
@@ -4,6 +4,12 @@
 #include "Board.h"
 #include "Tile.h"
 
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
 Game::Game(Player *player1, Player *player2, LinkedList *bag, Board *board, std::string currentPlayer)
 {
     this->player1 = player1;
@@ -21,7 +27,13 @@ Game::~Game()
     delete board;
 }
 
-void Game::executeGameplay() {
+void Game::executeGameplay() 
+{
+    bool stop = false;
+    while(!stop&&!player1->getHand()->isEmpty()&&player2->getHand()->isEmpty()&&!bag->isEmpty()) 
+    {
+        cout << player1->getName() << ", it's your turn" << endl;
+    }
 
 }
 
