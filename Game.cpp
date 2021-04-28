9c3b6c72c32788a21fec11cf49601a79692c126b
[Sidhra Fernando-Plant s3873756] <s3873756@student.rmit.edu.au>
2021-04-28 16:27:25 +1000

    Add print statements to show user game status

--- Game.cpp
+++ Game.cpp
@@ -9,6 +9,7 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
 Game::Game(Player *player1, Player *player2, LinkedList *bag, Board *board, std::string currentPlayer)
 {
@@ -32,7 +33,14 @@ void Game::executeGameplay()
     bool stop = false;
     while(!stop&&!player1->getHand()->isEmpty()&&player2->getHand()->isEmpty()&&!bag->isEmpty()) 
     {
+        string command;
         cout << player1->getName() << ", it's your turn" << endl;
+        cout << "Score for " << player1->getName() << ": " << player1->getPoints() << endl;
+        cout << "Score for " << player2->getName() << ": " << player2->getPoints() << endl;
+        //print  board
+        cout << "Your hand is" << endl;
+        //print current players hand
+        
     }
 
 }
