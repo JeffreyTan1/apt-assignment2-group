90bedddcb0b3b6eba1e8ae91b348e84e22f6b55e
[Sidhra Fernando-Plant s3873756] <s3873756@student.rmit.edu.au>
2021-04-30 17:54:46 +1000

    Update placeTile in Board class to take pointer to a tile

--- Board.h
+++ Board.h
@@ -12,7 +12,7 @@ public:
     Board();
 
     ~Board();
-    void placeTile(Tile tile, int row, int col);
+    void placeTile(Tile *tile, int row, int col);
 
 private:
     vector<vector<Tile>> board;
